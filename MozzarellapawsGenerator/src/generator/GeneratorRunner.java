69a64a4585ab0cd93eb10ba600234ad16ea75bcd
CRun7789 <chantelle.runyan@gmail.com>
2025-04-28 00:34:13 -0500

    Reapply "Added code (but with a src folder this time)"
    This reverts commit 09175ae63f98a57fe3c6f30b6023c49855bf2bae.

---
+++ MozzarellapawsGenerator/src/generator/GeneratorRunner.java
@@ -0,0 +1,141 @@
+package generator;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Scanner;
+
+public class GeneratorRunner {
+
+    public static String myName;
+
+    /**
+     * Generates an Eight-Mozzarellapaws-type name based on certain parameters.
+     * 
+     * @param  min             Minimum number of first name
+     * @param  max             Maximum number of first name
+     * @param  useAllCheeses   Whether to use list of all cheeses or just common ones
+     * @param  useAllBodyParts Whether to use list of all body parts or just reasonable ones
+     * @return                 a complete name
+     */
+    private static String generateName(int min, int max, boolean useAllCheeses, boolean useAllBodyParts) {
+        StringBuilder name = new StringBuilder();
+
+        name.append(OptionsHolder.getNumber(min, max));
+        name.append(" ");
+        name.append(OptionsHolder.getCheese(useAllCheeses));
+        name.append(OptionsHolder.getBodyPart(useAllBodyParts));
+
+        return name.toString();
+    }
+
+    /**
+     * Generates an Eight-Mozzarellapaws-type name using simple default parameters.
+     * 
+     * @return a complete name
+     */
+    private static String generateNameSimple() {
+        return generateName(0, 100, false, false);
+    }
+
+    /**
+     * Generates an Eight-Mozzarellapaws-type name using suggested default parameters.
+     * 
+     * @return a complete name
+     */
+    private static String generateNameSuggested() {
+        return generateName(0, 100, false, true);
+    }
+
+    /**
+     * Adds the generated name to the list of all previously generated names.
+     */
+    private static void addNameToTextFile() {
+        String filepath = "./all_generated_names";
+        String nameToAdd = myName + "\n";
+        try {
+            Files.write(Paths.get(filepath), nameToAdd.getBytes(), StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            System.out.println("...Awww, but I can't add it to the list of all generated names right meow. Sad day :(");
+        }
+    }
+
+    /**
+     * Calls all the important stuff and walks the user through getting input. Separate from main so I can loop easier.
+     * 
+     * @param s System.in Scanner
+     */
+    private static void getParamsAndGenerateName(Scanner s) {
+        System.out.println(
+                "(If you wanna skip right to the final name with SIMPLE DEFAULTS, just press enter. For SUGGESTED DEFAULTS, type '8'.)");
+        System.out
+                .println("Otherwise, if you wanna put in parameters yourself, type something else! Anything! Get crazy with it! So long as it's not a newline.");
+
+        String input = s.nextLine();
+        if (input.length() < 1) {
+            // use simple defaults
+            myName = generateNameSimple();
+        } else if (input.equals("8")) {
+            // use suggested defaults
+            myName = generateNameSuggested();
+        } else {
+            // prompt for parameters
+            System.out.println("Great, you're still here! That means you're looking for some customization!");
+            int min = InputValidation.queryInt(s,
+                    "First off, I need the MINIMUM NUMBER for my first name. Negatives are allowed! But, uh, maybe don't put negative 999 william... that might be too high...");
+            int max = InputValidation.queryInt(s,
+                    "Got it! Now, the MAXIMUM NUMBER my first name could be. Again, please not 999 william!!");
+
+            System.out.println(
+                    "\nFor my next two tricks- I mean, next parts of my name- I'm gonna need a boolean. '0' is gonna be FALSE/NO, and '1' is gonna stand for TRUE/YES.\n");
+            System.out.println(
+                    "In terms of CHEESE, I have two lists of options: the first has one william types of cheese from all around the world! There are a lot!!");
+            System.out
+                    .println("The second has types of cheeses that you're more likely to find in a US grocery store.");
+            boolean useAllCheeses = InputValidation.queryBool(s,
+                    "Wanna use the GIANT LIST? Remember, '1' for YES or '0' for NO.");
+            boolean useAllBodyParts = InputValidation.queryBool(s,
+                    "\nSwagalicious! Now for BODY PARTS. Shall I draw from the giant list (type '1'), or the reasonable one (type '0')?");
+
+            myName = generateName(min, max, useAllCheeses, useAllBodyParts);
+        }
+
+        System.out.println("\n\nOkay, time for the grand reveal! My new name is...\n\n");
+        System.out.println("\n" + myName + "!\n");
+        System.out.println("Hey, that's got a ring to it!");
+
+        addNameToTextFile();
+    }
+    
+    /**
+     * The typical main method - gets user input, calls all the methods, and presents the results to the user.
+     * 
+     * @param args This method does not accept any arguments.
+     */
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        // greet user
+        System.out.println(
+                OptionsHolder.getGreeting() + " This is your host, an unnamed cat, and you're here to name me!");
+        System.out.println(
+                "...Well, at least to pick the conditions under which my name is randomly chosen. Close enough for me!");
+        System.out.println(
+                "\nSo, you've gotta enter some info for me. We're basing this name off the icomic amd imeffable Eight Mozzarellapaws!");
+        System.out.println("Their name has three parts: a NUMBER, a CHEESE, and a BODY PART.");
+        
+        // create names as long as the user wants
+        boolean again = true;
+        while (again) {
+            getParamsAndGenerateName(s);
+            again = InputValidation.queryBool(s, "Wanna generate another? Type 1 for YES or 0 for NO!");
+        }
+        
+        // buh-bye
+        System.out.println("It was fun playing with you! Bye-bye! :3");
+        s.close();
+    }
+
+}
---
+++ MozzarellapawsGenerator/src/generator/NumToWordConverter.java
@@ -0,0 +1,154 @@
+package generator;
+
+import java.text.DecimalFormat;
+
+/**
+ * Converts a given int into capitalized word form. Original: https://www.rgagnon.com/javadetails/java-0426.html
+ */
+public class NumToWordConverter {
+
+    private static final String[] tensNames = { "", " Ten", " Twenty", " Thirty", " Forty", " Fifty", " Sixty",
+            " Seventy", " Eighty", " Ninety" };
+
+    private static final String[] numNames = { "", " One", " Two", " Three", " Four", " Five", " Six", " Seven",
+            " Eight", " Nine", " Ten", " Eleven", " Twelve", " Thirteen", " Fourteen", " Fifteen", " Sixteen",
+            " Seventeen", " Eighteen", " Nineteen" };
+
+    private NumToWordConverter() {
+    }
+
+    private static String convertLessThanOneThousand(int number) {
+        String soFar;
+
+        if (number % 100 < 20) {
+            soFar = numNames[number % 100];
+            number /= 100;
+        } else {
+            soFar = numNames[number % 10];
+            number /= 10;
+
+            // dashes in numbers like Twenty-Three are added here
+            if (soFar.length() > 1) {
+                soFar =  tensNames[number % 10] + "-" + soFar.substring(1);
+            } else {
+                soFar =  tensNames[number % 10] + "-" + soFar;
+            }
+            number /= 10;
+        }
+        if (number == 0) {
+            return soFar;
+        }
+        return numNames[number] + " Hundred" + soFar;
+    }
+
+    public static String convertNegative(long number) {
+        return "Negative " + convert(Math.abs(number));
+    }
+    
+    public static String convert(long number) {
+        // -999 999 999 999 to 999 999 999 999
+        if (number == 0) {
+            return "Zero";
+        }
+        
+        if (number < 0) {
+            return convertNegative(number);
+        }
+
+        String snumber = Long.toString(number);
+
+        // pad with "0"
+        String mask = "000000000000";
+        DecimalFormat df = new DecimalFormat(mask);
+        snumber = df.format(number);
+
+        // XXXnnnnnnnnn
+        int billions = Integer.parseInt(snumber.substring(0, 3));
+        // nnnXXXnnnnnn
+        int millions = Integer.parseInt(snumber.substring(3, 6));
+        // nnnnnnXXXnnn
+        int hundredThousands = Integer.parseInt(snumber.substring(6, 9));
+        // nnnnnnnnnXXX
+        int thousands = Integer.parseInt(snumber.substring(9, 12));
+
+        String tradBillions;
+        switch (billions) {
+            case 0:
+                tradBillions = "";
+                break;
+            case 1:
+                tradBillions = convertLessThanOneThousand(billions) + " Billion ";
+                break;
+            default:
+                tradBillions = convertLessThanOneThousand(billions) + " Billion ";
+        }
+        String result = tradBillions;
+
+        String tradMillions;
+        switch (millions) {
+            case 0:
+                tradMillions = "";
+                break;
+            case 1:
+                tradMillions = convertLessThanOneThousand(millions) + " Million ";
+                break;
+            default:
+                tradMillions = convertLessThanOneThousand(millions) + " Million ";
+        }
+        result = result + tradMillions;
+
+        String tradHundredThousands;
+        switch (hundredThousands) {
+            case 0:
+                tradHundredThousands = "";
+                break;
+            case 1:
+                tradHundredThousands = "One Thousand ";
+                break;
+            default:
+                tradHundredThousands = convertLessThanOneThousand(hundredThousands) + " Thousand ";
+        }
+        result = result + tradHundredThousands;
+
+        String tradThousand;
+        tradThousand = convertLessThanOneThousand(thousands);
+        result = result + tradThousand;
+
+        // remove extra spaces!
+        return result.replaceAll("^\\s+", "").replaceAll("\\b\\s{2,}\\b", " ");
+    }
+    
+
+    /**
+     * Used for testing.
+     * 
+     * @param args This method does not accept any arguments.
+     */
+    public static void main(String[] args) {
+        int zero = 0;
+        System.out.println("Got: " + convert(-123456));
+        System.out.println("Got: " + convert(-12));
+        System.out.println("Got: " + convert(zero));
+        System.out.println("Got: " + convert(1));
+        System.out.println("Got: " + convert(8));
+        System.out.println("Got: " + convert(16));
+        System.out.println("Got: " + convert(72));
+        System.out.println("Got: " + convert(133));
+        System.out.println("Got: " + convert(100));
+        System.out.println("Got: " + convert(118));
+        System.out.println("Got: " + convert(200));
+        System.out.println("Got: " + convert(219));
+        System.out.println("Got: " + convert(800));
+        System.out.println("Got: " + convert(801));
+        System.out.println("Got: " + convert(1316));
+        System.out.println("Got: " + convert(1000000));
+        System.out.println("Got: " + convert(2000000));
+        System.out.println("Got: " + convert(3000200));
+        System.out.println("Got: " + convert(700000));
+        System.out.println("Got: " + convert(9000000));
+        System.out.println("Got: " + convert(9001000));
+        System.out.println("Got: " + convert(123456789));
+        System.out.println("Got: " + convert(2147483647));
+        System.out.println("Got: " + convert(3000000010L));
+    }
+}
